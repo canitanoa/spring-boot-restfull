41b1da5489115c1fc1b3b3294098395d33b3204f
Adrián Canitano <acanitano@gmail.com>
2021-06-13 02:51:46 -0300

    Initial commit

---
+++ src/test/java/ar/com/cani/SpringBootRestfullApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.cani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestfullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
